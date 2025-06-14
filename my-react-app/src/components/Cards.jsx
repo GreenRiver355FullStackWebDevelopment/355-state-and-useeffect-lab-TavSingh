bfd87607bc3e094c626a5189157043e2b3cda4d0
TavSingh <tavparsadsingh2@gmail.com>
2025-06-13 20:07:28 -0700

    Finished changes using MUI for cards

--- my-react-app/src/components/Cards.jsx
+++ my-react-app/src/components/Cards.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import CardDetail from "./CardDetail.jsx";
+import CardDetail from "./CardDetail";
+import { Box, Button } from "@mui/material";
 
 const Cards = ({ pokemonList }) => {
   const [selected, setSelected] = useState(null);
@@ -11,12 +12,36 @@ const Cards = ({ pokemonList }) => {
   };
 
   return (
-    <div className="cards">
-      {pokemonList.map((p, index) => (
-        <div key={index} className="card" onClick={() => onPokemonClick(p.url)}>
-          {p.name}
-        </div>
-      ))}
+    <div>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(5, 1fr)",
+          backgroundColor: "#222",
+          gap: 1.5,
+          padding: 3,
+          borderRadius: 3,
+          maxWidth: "750px",
+          margin: "0 auto",
+        }}
+      >
+        {pokemonList.map((p, index) => (
+          <Button
+            key={index}
+            onClick={() => onPokemonClick(p.url)}
+            sx={{
+              backgroundColor: "#444",
+              color: "white",
+              "&:hover": {
+                backgroundColor: "#666",
+              },
+            }}
+          >
+            {p.name.charAt(0).toUpperCase() + p.name.slice(1)}
+          </Button>
+        ))}
+      </Box>
+
       {selected && <CardDetail pokemon={selected} />}
     </div>
   );
